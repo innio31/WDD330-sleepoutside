ad332b4d8717466f615c3abfe9cc67c39990ca61
Emmanuel Ademuyiwa <samadeimmanuel@yahoo.com>
2026-09-25 08:15:14 +0100

    W03 team activity: dynamic header/footer + ShoppingCart refactor

---
+++ src/js/ShoppingCart.mjs
@@ -0,0 +1,33 @@
+import { getLocalStorage, renderListWithTemplate } from "./utils.mjs";
+
+function cartItemTemplate(item) {
+  return `<li class="cart-card divider">
+    <a href="/product_pages/index.html?product=${item.Id}" class="cart-card__image">
+      <img
+        src="${item.Image.replace("../", "/")}"
+        alt="${item.Name}"
+      />
+    </a>
+    <a href="/product_pages/index.html?product=${item.Id}">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+  </li>`;
+}
+
+export default class ShoppingCart {
+  constructor(listElement) {
+    this.listElement = listElement;
+  }
+
+  init() {
+    const cartItems = getLocalStorage("so-cart") || [];
+    this.renderList(cartItems);
+  }
+
+  renderList(list) {
+    renderListWithTemplate(cartItemTemplate, this.listElement, list);
+  }
+}
--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,11 @@
-import { getParam, setLocalStorage } from "./utils.mjs";
+import {
+  getParam,
+  setLocalStorage,
+  getLocalStorage,
+  loadHeaderFooter,
+} from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+
 
 const dataSource = new ProductData("tents");
 
@@ -32,7 +38,14 @@ function productDetailsTemplate(product) {
 }
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cart = getLocalStorage("so-cart") || [];
+  if (!Array.isArray(cart)) {
+    // migrate any previously-saved single object into an array
+    setLocalStorage("so-cart", [cart, product]);
+    return;
+  }
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
 
 function addToCartHandler(product) {
@@ -52,3 +65,5 @@ async function init() {
 }
 
 init();
+
+loadHeaderFooter();
